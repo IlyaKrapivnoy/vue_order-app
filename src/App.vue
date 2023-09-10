f91ba1c55fb2f92be92f0a870e1a1a168510648c
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-09-10 15:46:53 +0100

    add jobs

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,43 @@
 <template>
-  <div class="app">Order Now App</div>
+  <div class="app">
+    <p>{{ jobs[0].location }}</p>
+  </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
+  import { defineComponent, ref } from 'vue';
+  import Job from '@/types/Job';
 
   export default defineComponent({
     name: 'App',
     components: {},
-  })
+    setup() {
+      // const age = ref<string | number>(25)
+      const jobs = ref<Job[]>([
+        {
+          title: 'JavaScript Developer',
+          location: 'lisbon, Portugal',
+          salary: 50600,
+          id: '1',
+        },
+        {
+          title: 'Vue Developer',
+          location: 'Porto, Portugal',
+          salary: 40200,
+          id: '2',
+        },
+        {
+          title: 'React Developer',
+          location: 'Berlin, Germany',
+          salary: 40500,
+          id: '3',
+        },
+      ]);
+
+      return { jobs };
+    },
+    methods: {},
+  });
 </script>
 
 <style></style>
