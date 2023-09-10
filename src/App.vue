09270d5650b8d6fc80515c99a5c8c091640c0201
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-09-10 16:47:20 +0100

    separate jobs data

--- src/App.vue
+++ src/App.vue
@@ -14,30 +14,9 @@
 
 <script setup lang="ts">
   import { ref } from 'vue';
-  import Job from '@/types/Job';
   import JobsList from '@/components/JobsList.vue';
   import OrderTerm from '@/types/OderTerm';
-
-  const jobs = ref<Job[]>([
-    {
-      title: 'JavaScript Developer',
-      location: 'lisbon, Portugal',
-      salary: 50600,
-      id: '1',
-    },
-    {
-      title: 'Vue Developer',
-      location: 'Porto, Portugal',
-      salary: 40200,
-      id: '2',
-    },
-    {
-      title: 'React Developer',
-      location: 'Berlin, Germany',
-      salary: 40500,
-      id: '3',
-    },
-  ]);
+  import jobs from '@/data/jobs';
 
   const order = ref<OrderTerm>('title');
 
---
+++ src/data/jobs.ts
@@ -0,0 +1,24 @@
+import Job from '@/types/Job';
+
+const jobs: Job[] = [
+  {
+    title: 'JavaScript Developer',
+    location: 'lisbon, Portugal',
+    salary: 50600,
+    id: '1',
+  },
+  {
+    title: 'Vue Developer',
+    location: 'Porto, Portugal',
+    salary: 40200,
+    id: '2',
+  },
+  {
+    title: 'React Developer',
+    location: 'Berlin, Germany',
+    salary: 40500,
+    id: '3',
+  },
+];
+
+export default jobs;
