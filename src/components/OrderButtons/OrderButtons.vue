5c2b9ffdd7c4353ed0e2f7cecae5035589201e8f
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-09-15 17:29:04 +0100

    add active btn

--- src/components/OrderButtons/OrderButtons.vue
+++ src/components/OrderButtons/OrderButtons.vue
@@ -4,6 +4,7 @@
       v-for="(term, index) in orderTerms"
       :key="index"
       @click="handleClick(term)"
+      :class="{ active: term === selectedTerm }"
     >
       Order by {{ term }}
     </button>
@@ -11,14 +12,16 @@
 </template>
 
 <script setup lang="ts">
-  import { defineEmits } from 'vue';
+  import { defineEmits, ref } from 'vue';
   import OrderTerm from '@/types/OderTerm';
 
   const orderTerms: OrderTerm[] = ['location', 'title', 'salary'];
+  const selectedTerm = ref<OrderTerm>('title');
 
   const emit = defineEmits(['onOrderClick']);
 
   const handleClick = (term: OrderTerm) => {
+    selectedTerm.value = term;
     emit('onOrderClick', term);
   };
 </script>
@@ -26,5 +29,10 @@
 <style scoped>
   .order {
     margin-top: 20px;
+  }
+
+  .active {
+    background-color: #1195c9;
+    color: white;
   }
 </style>
