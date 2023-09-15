cd71f34e00a57488ee727b7e03277356833fdfb2
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-09-15 16:37:49 +0100

    test onOrderClick with dif values

--- src/components/OrderButtons/__tests__/OrderButtons.spec.js
+++ src/components/OrderButtons/__tests__/OrderButtons.spec.js
@@ -12,4 +12,25 @@ describe('OrderButtons.vue', () => {
     expect(wrapper.emitted('onOrderClick')).toBeTruthy();
     expect(wrapper.emitted('onOrderClick')[0]).toEqual(['location']);
   });
+
+  it('emits the "onOrderClick" event with "location" when the "Order by location" button is clicked', async () => {
+    const wrapper = mount(OrderButtons);
+    await wrapper.findAll('button').at(0).trigger('click');
+    expect(wrapper.emitted('onOrderClick')).toBeTruthy();
+    expect(wrapper.emitted('onOrderClick')[0]).toEqual(['location']);
+  });
+
+  it('emits the "onOrderClick" event with "title" when the "Order by title" button is clicked', async () => {
+    const wrapper = mount(OrderButtons);
+    await wrapper.findAll('button').at(1).trigger('click');
+    expect(wrapper.emitted('onOrderClick')).toBeTruthy();
+    expect(wrapper.emitted('onOrderClick')[0]).toEqual(['title']);
+  });
+
+  it('emits the "onOrderClick" event with "salary" when the "Order by salary" button is clicked', async () => {
+    const wrapper = mount(OrderButtons);
+    await wrapper.findAll('button').at(2).trigger('click');
+    expect(wrapper.emitted('onOrderClick')).toBeTruthy();
+    expect(wrapper.emitted('onOrderClick')[0]).toEqual(['salary']);
+  });
 });
